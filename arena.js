be96fbb7e625e57544a8b223bec4cee49db5835f
Ben Ebel <bge1234@gmail.com>
2015-11-24 16:36:31 -0700

    added different attacks for each character and randomized their selection

--- arena.js
+++ arena.js
@@ -1,10 +1,39 @@
 function fight(attacker, defender) {
-  console.log(attacker.name + " vs. " + defender.name);
-  var effectivenessMult = effectiveness(attacker.type, defender.type);
-  var damage = 50 * (attacker.attack/defender.defense) * effectivenessMult;
-  console.log(damage + " damage was done to " + defender.name);
+  console.log("Fight: " + attacker.name + " vs. " + defender.name);
+  var attackType = selectAttack(attacker);
+  var damage = damageCalc(attacker, defender, attackType);
   subtractHealth(defender, damage);
   return;
+}
+
+function selectAttack(attacker) {
+  if (Math.random() <= .33) {
+    console.log(attacker.name + " used " + attacker.attack1.name);
+    return "one";
+  }
+  else if (Math.random() > .33 && Math.random() <= .67) {
+    console.log(attacker.name + " used " + attacker.attack2.name);
+    return "two";
+  }
+  else {
+    console.log(attacker.name + " used " + attacker.special_attack.name);
+    return "special";
+  }
+}
+
+function damageCalc(attacker, defender, attackType) {
+  var damage = 0;
+  var effectivenessMult = effectiveness(attacker.type, defender.type);
+
+  if (attackType === "one")
+    damage = (attacker.attack1.power - defender.defense) * effectivenessMult;
+  else if (attackType === "two")
+    damage = (attacker.attack2.power - defender.defense) * effectivenessMult;
+  else
+    damage = (attacker.special_attack.power - defender.defense) * effectivenessMult;
+
+  console.log(damage + " damage was done to " + defender.name);
+  return damage;
 }
 
 function subtractHealth(name, damage) {
@@ -13,8 +42,8 @@ function subtractHealth(name, damage) {
     console.log(name.name + " is dead!");
   }
   else {
-    console.log(name.name + "'s remaining health is " + name.health);
     name.health -= damage;
+    console.log(name.name + "'s remaining health is " + name.health);
   }
   return;
 }
@@ -22,74 +51,79 @@ function subtractHealth(name, damage) {
 function effectiveness(attackerType, defenderType) {
   if (attackerType === "earth") {
     if (defenderType === "earth") {
-      console.log("earth has normal effectiveness against earth");
+      console.log("Earth has normal effectiveness against earth");
       return 1;
     }
     else if (defenderType === "wind") {
-      console.log("earth has low effectiveness against wind");
+      console.log("Earth has low effectiveness against wind");
       return .5;
     }
     else if (defenderType === "water") {
-      console.log("earth has low effectiveness against water");
+      console.log("Earth has low effectiveness against water");
       return .5;
     }
     else {
-      console.log("earth has high effectiveness against fire");
+      console.log("Earth has high effectiveness against fire");
       return 2;
     }
   }
   else if (attackerType === "wind") {
     if (defenderType === "earth") {
-      console.log("wind has low effectiveness against earth");
+      console.log("Wind has low effectiveness against earth");
       return .5;
     }
     else if (defenderType === "wind") {
-      console.log("wind has normal effectiveness against wind");
+      console.log("Wind has normal effectiveness against wind");
       return 1;
     }
     else if (defenderType === "water") {
-      console.log("wind has high effectiveness against water");
+      console.log("Wind has high effectiveness against water");
       return 2;
     }
     else {
-      console.log("wind has high effectiveness against fire");
+      console.log("Wind has high effectiveness against fire");
       return 2;
     }
   }
   else if (attackerType === "water") {
     if (defenderType === "earth") {
-      console.log("water has high effectiveness against earth");
+      console.log("Water has high effectiveness against earth");
       return 2;
     }
     else if (defenderType === "wind") {
-      console.log("water has low effectiveness against wind");
+      console.log("Water has low effectiveness against wind");
       return .5;
     }
     else if (defenderType === "water") {
-      console.log("water has normal effectiveness against water");
+      console.log("Water has normal effectiveness against water");
       return 1;
     }
     else {
-      console.log("water has high effectiveness against fire");
+      console.log("Water has high effectiveness against fire");
       return 2;
     }
   }
   else {
     if (defenderType === "earth") {
-      console.log("fire has low effectiveness against earth");
+      console.log("Fire has low effectiveness against earth");
       return .5;
     }
     else if (defenderType === "wind") {
-      console.log("fire has low effectiveness against wind");
+      console.log("Fire has low effectiveness against wind");
       return .5;
     }
     else if (defenderType === "water") {
-      console.log("fire has low effectiveness against water");
+      console.log("Fire has low effectiveness against water");
       return .5;
     }
     else {
-      console.log("fire has normal effectiveness against fire");
+      console.log("Fire has normal effectiveness against fire");
       return 1;
     }
   }
+}
+
+function resetKongHealth() {
+  kong.health = 200;
+  return;
 }
--- characters.js
+++ characters.js
@@ -1,15 +1,51 @@
 var zilla = {
   name: "Godzilla",
   type: "water",
-  attack: 80,
-  defense: 60,
-  health: 100
+  attack: 0,
+  attack1: {
+    type: "one",
+    name: "Tail Whip",
+    power: 40,
+    chance: .7,
+  },
+  attack2: {
+    type: "two",
+    name: "Fire Breath",
+    power: 80,
+    chance: .2,
+  },
+  special_attack: {
+    type: "special",
+    name: "Tidal Wave",
+    power: 2000,
+    chance: .05,
+  },
+  defense: 6,
+  health: 250
 };
 
 var kong = {
   name: "King Kong",
   type: "earth",
-  attack: 50,
-  defense: 70,
-  health: 100
+  attack: 0,
+  attack1: {
+    type: "one",
+    name: "Bite",
+    power: 30,
+    chance: .7,
+  },
+  attack2: {
+    type: "two",
+    name: "Pound",
+    power: 80,
+    chance: .2,
+  },
+  special_attack: {
+    type: "special",
+    name: "Body Slam",
+    power: 2000,
+    chance: .05,
+  },
+  defense: 8,
+  health: 200
 };
